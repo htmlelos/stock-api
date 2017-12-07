194b5b9420d7c42f8b21e71f39ed650f022d3ad9
sergiolucero73 <sergiolucero73@gmail.com>
2017-12-07 20:55:09 -0300

    fix document/accept-item endpoint

--- controllers/document.js
+++ controllers/document.js
@@ -331,15 +331,9 @@ const acceptItem = (request, response) => {
           let error = { code: 400, message: 'La recepcion ya se encuentra confirmada', data: null }
           return Promise.reject(error)
         }
-        console.log('BODY-ITEMS', items)
-        // return document.detail.map(item => {
-        //   console.log('DETAIL-ITEM', item)
-        //   return (items.includes(item._id.toString()) && item.status !== 'ACEPTADO') ? Object.assign(item, { status: 'ACEPTADO' }) : item
-        // })
         return items.map(item => {
-          return document.detail.find(x => { (x._id === item._id && item.status !== 'ACEPTADO') ? Object.assign(item, {status: 'ACEPTADO'}): item})
-        })
-        
+            return (document.detail.find(detail => detail._id.toString() === item._id.toString()) !== undefined) ? Object.assign(item, {status: 'ACEPTADO'}) : item
+        })        
       } else {
         let error = { code: 404, message: 'no se pudo encontrar el documento', data: null }
         return Promise.reject(error)
